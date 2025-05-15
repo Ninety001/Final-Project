eae8201ee4000267df78525c22aa4bd648b669c6
Ninety001 <sanawuth.homc@bumail.net>
2025-05-16 03:48:01 +0700

    Main UI Setup

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class UIManager : MonoBehaviour
@@ -10,6 +11,13 @@ public class UIManager : MonoBehaviour
     public TextMeshProUGUI coinText;
     public GameObject newHighScoreText;
 
+    public GameObject mainMenuPanel;
+    public GameObject gameOverPanel;
+    public GameObject historyPanel;
+    public GameObject optionPanel;
+
+    public Toggle musicToggle;
+
     private bool shownNewHighScore = false;
 
     void Awake()
@@ -17,10 +25,40 @@ public class UIManager : MonoBehaviour
         Instance = this;
     }
 
+    void Start()
+    {
+        ShowMainMenu();
+        musicToggle.onValueChanged.AddListener(ToggleMusic);
+    }
+
+    public void ShowMainMenu()
+    {
+        mainMenuPanel.SetActive(true);
+        gameOverPanel.SetActive(false);
+        historyPanel.SetActive(false);
+        optionPanel.SetActive(false);
+    }
+
+    public void ShowHistory() { mainMenuPanel.SetActive(false); historyPanel.SetActive(true); }
+
+    public void ShowOption()
+    {
+        mainMenuPanel.SetActive(false);
+        optionPanel.SetActive(true);
+        musicToggle.isOn = AudioListener.volume > 0;
+    }
+
+    public void ToggleMusic(bool isOn)
+    {
+        AudioListener.volume = isOn ? 1 : 0;
+    }
+
     public void UpdateCoinText(int count)
     {
         coinText.text = $"Coins: {count}";
     }
+
+    public void ReturnToMenu() => ShowMainMenu();
 
     void Update()
     {
